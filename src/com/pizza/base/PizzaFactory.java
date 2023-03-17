722463b3c55bbd8861a025c6f9603371f15b6574
sameer patil <sbpatil29@gmail.com>
2023-03-17 13:39:55 +0530

    changes in the structure of pizza factory class

--- src/com/pizza/base/PizzaFactory.java
+++ src/com/pizza/base/PizzaFactory.java
@@ -1,8 +1,12 @@
 package com.pizza.base;
-public interface PizzaFactory {
+
+import java.util.ArrayList;
+
+public interface PizzaFactory{
      Pizza createPizza();
-     int checkInventory();
-     Order createOrder(Pizza p);
-     Order createOrder(Pizza p,Sides s);
      Sides createSides();
+     Crust createCrust();
+     Toppings createToppings();
+     Order createOrder(ArrayList<Pizza> p);
+     Order createOrder(ArrayList<Pizza> p,ArrayList<Sides> s);
     }
