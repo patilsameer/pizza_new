1e0bcc8f1d5e2c0fe56274d5fb43782d52948186
sameer patil <sbpatil29@gmail.com>
2023-03-18 13:12:43 +0530

    PizzaTest

---
+++ src/com/pizza/test/PizzaTest.java
@@ -0,0 +1,46 @@
+package com.pizza.test;
+
+import java.beans.BeanProperty;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pizza.base.Crust;
+import com.pizza.base.Pizza;
+import com.pizza.base.Toppings;
+import com.pizza.operation.CrustType;
+import com.pizza.operation.InventoryException;
+import com.pizza.operation.MyToppings;
+import com.pizza.operation.PizzaClass;
+
+import static org.junit.Assert.assertEquals;
+
+public class PizzaTest {
+   
+    Pizza pizza;
+    Crust crust;
+    Toppings toppings;
+    @Before
+    public void setUpPizza()throws InventoryException{
+        pizza=new PizzaClass();
+        crust=new CrustType();
+        pizza.setCrust(crust);
+        toppings=new MyToppings();
+        toppings.setPrice(100);
+        toppings.setToppingsName("Extra Chees");
+        pizza.setPizzaName("Italian");
+        pizza.setPizzaPrice(100);
+        pizza.setSize(1);
+        pizza.setToppings(toppings);    
+    }
+
+    @Test
+    public void pizzaTest()throws InventoryException{
+        assertEquals(pizza.getCrustName(),crust.getCrustName());
+        assertEquals(pizza.getCrust(),crust);
+        assertEquals(pizza.getPizzaName(),"Italian");
+        assertEquals(pizza.getPizzaPrice(),200);//pizza + toppings
+        assertEquals(pizza.getSize(),1);
+        assertEquals(pizza.getToppingName(),toppings.getToppingName());
+    }
+}
--- src/com/pizza/test/TestRunner.java
+++ src/com/pizza/test/TestRunner.java
@@ -2,11 +2,14 @@ package com.pizza.test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 
+
 public class TestRunner {
 
     public static void main(String [] args){
         Result result=JUnitCore.runClasses(InventoryTest.class);
         System.out.println(result.wasSuccessful());
+        Result result1=JUnitCore.runClasses(PizzaTest.class);
+        System.out.println(result1.wasSuccessful());
      }
     
 }
